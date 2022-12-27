ce43142cb99c78b5d0fd98b4e0ce04f4867a1620
Marcin <contact@mmskl.net>
2022-12-27 04:51:20 +0100

    Add rest of the menu items and create stateful class for game

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,14 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Manual reflex analyzer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: Game(title: 'GAME'),
     );
   }
 }
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: NavDrawer(),
-      appBar: AppBar(
-        title: Text('Side menu'),
-      ),
-      body: Center(
-        child: Text('Side Menu Tutorial'),
-      ),
-    );
-  }
-}
-
-
 
 class NavDrawer extends StatelessWidget {
   @override
@@ -49,7 +32,163 @@ class NavDrawer extends StatelessWidget {
             leading: Icon(Icons.input),
             title: Text('Game'),
             onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.verified_user),
+            title: Text('Hall of Fame'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('About'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => About()));
+            },
+          ),
         ],
+      ),
+    );
+  }
+}
+
+
+class Game extends StatefulWidget {
+  const Game({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the Game object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      drawer: NavDrawer(),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+
+class About extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<String> authors = [
+      'Marcin Moskal',
+      'Kacper Kromka',
+      'Piotr Badełek',
+      'Szymon Manecki',
+      'Przemysław Frąszczak',
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About'),
+      ),
+      body: Container(
+        alignment: Alignment.centerRight,
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: ListView.builder(
+            itemCount: authors.length,
+            prototypeItem: ListTile(
+              title: Text(authors.first),
+            ),
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(authors[index]),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HallOfFame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HallOfFame'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('HallOfFame Page'),
+        ),
       ),
     );
   }
